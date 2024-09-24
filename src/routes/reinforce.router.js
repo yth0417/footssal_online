a5ff03bd8fa9d54343289cd40648c3d6f1879d8a
bss1999 <59215767+bss1999@users.noreply.github.com>
2024-09-24 14:19:10 +0900

    강화 API 병합
    각각 스키마에 맞게 변경

--- src/routes/reinforce.router.js
+++ src/routes/reinforce.router.js
@@ -1,22 +1,8 @@
 import express from "express";
-import authMiddleware from "../middlewares/auth.middleware.js";
 import { prisma } from "../utils/prisma/index.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
-
-// 강화기능
-
-// 비용 :
-//     1. 강화비용은 기본 5천원
-//     1-1. 강화단계에따라 비용은 점점 증가
-//     2. 보유숫자가 2개이상일시 보유숫자를 줄여서 강화비용은 무료로 진행 가능
-
-// 강화내용 :
-//     1. 강화시 능력치 10%씩 복리로 강화
-
-// 강화실패 :
-//     1. 3강까지는 실패시 유지
-//     2. 4강부터는 실패시 유지하지만 터지는확률도 생긴다
 
 
 //병합을위한 체크사항
@@ -37,7 +23,7 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
     //강화시 중복 보유선수를 소모하여 무료로 강화를 진행할지 묻는 질문
     //YES 면 보유선수 소모
     //그이외의 답은 모두 NO 처리
-    const { costQ } = req.body;
+    const { costQuestion } = req.body;
 
     //어떤 선수를 강화할지 선택함
     const { playerId } = req.params;
@@ -46,7 +32,7 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
     const cost = 5000;
 
     //강화할 선수의 정보를 불러옴
-    const player = await prisma.playerWaitingList.findFirst({
+    const player = await prisma.playerWaitingLists.findFirst({
       where: {
         userId: user.userId,
         playerId: +playerId,
@@ -68,14 +54,14 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
 
 
 
-    //강화확률
+    //강화 확률
     const reinforceRate = Math.floor(Math.random() * 100);
     //강화 성공확률 초기화
     let successRate = 0;
 
-    //파괴확률
+    //파괴 확률
     const reinforceBreakRate = Math.floor(Math.random() * 100);
-    //터지는것에 성공(?)하는 확률 초기화
+    //파괴 확률 초기화
     let breakRate = 0;
 
     //선수의 강화수치에따른 확률 고정값 설정
@@ -92,7 +78,7 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
         break;
       case 4:
         successRate = 40;
-        //터질확률 = 10%
+        //파괴확률 = 10%
         breakRate = 10;
         break;
       case 5:
@@ -128,24 +114,24 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
     // await prisma.$transaction(async (tx) => { //트랜잭션 시작점
 
       //중복소유한 선수를 제물로 강화비 면제 | 그냥 재화 5천원 지불후 강화 선택 분기문
-      if (costQ === "YES") {
-        await prisma.PlayerWaitingList.update({
+      if (costQuestion === "YES") {
+        await prisma.playerWaitingLists.update({
             where: {
-                playerListId: player.playerListId,
+              playerWaitingListsId: player.playerWaitingListsId,
             },
           data: {
             count: { decrement: 1 },
           },
         });
       } else {
-        if (cost > userInfo.cash) {
-            throw new Error("재화가 부족합니다!")
-            // return res
-            //   .status(400)
-            //   .json({
-            //     message:
-            //       "재화가 부족합니다!",
-            //   });
+        if (cost > userInfo.money) {
+            // throw new Error("재화가 부족합니다!")
+            return res
+              .status(400)
+              .json({
+                message:
+                  "재화가 부족합니다!",
+              });
         }
 
         await prisma.users.update({
@@ -153,7 +139,7 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
             userId: user.userId,
           },
           data: {
-            cash: { decrement: cost },
+            money: { decrement: cost },
           },
         });
       }
@@ -162,7 +148,7 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
       //[강화 실패] 만약 선수를 강화하다가 터진다면
       if (reinforceBreakRate < breakRate) {
         //초기(1강 상태)의 능력치를 가져오기위해 선수테이블에서 같은 선수의 정보를 불러옴
-        const fail_player = await prisma.player.findFirst({
+        const fail_player = await prisma.Players.findFirst({
           where: {
             playerId: +playerId,
           },
@@ -170,18 +156,18 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
 
         //보유갯수가 줄어야하는데 보유한 선수의 갯수가 1개라면 보유한게없어야 하므로 데이터 자체를 삭제
         if (player.count === 1) {
-          await prisma.playerWaitingList.delete({
+          await prisma.playerWaitingLists.delete({
             where: {
-              playerListId: player.playerListId,
+              playerWaitingListsId: player.playerWaitingListsId,
               userId: user.userId,
               playerId: +playerId,
             },
           });
         } else {
           // 보유갯수가 2개이상이면 보유갯수를 1만큼 줄이고 가져온 초기능력치를 적용
-          await prisma.playerWaitingList.update({
+          await prisma.playerWaitingLists.update({
             where: {
-              playerListId: player.playerListId,
+              playerWaitingListsId: player.playerWaitingListsId,
               userId: user.userId,
               playerId: +playerId,
             },
@@ -197,24 +183,24 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
           });
         }
 
-        throw new Error("강화 시도중 터졌습니다! 능력치,강화수치가 초기화됐습니다.")
-        // return res.status(200).json({
-        //     message: "강화 시도중 터졌습니다! 능력치,강화수치가 초기화됐습니다.",
-        //   });
+        // throw new Error("강화 시도중 터졌습니다! 능력치,강화수치가 초기화됐습니다.")
+        return res.status(200).json({
+            message: "강화 시도중 터졌습니다! 능력치,강화수치가 초기화됐습니다.",
+          });
       }
 
       if (reinforceRate > successRate) {
         //[강화 실패] 터지진 않았지만 강화에는 실패하여 강화수치는 유지
-        throw new Error("강화에 실패했습니다...")
-        // return res.status(200).json({ message: "강화에 실패했습니다..." });
+        // throw new Error("강화에 실패했습니다...")
+        return res.status(200).json({ message: "강화에 실패했습니다..." });
       }
 
       //[강화 성공] 모두 통과하여 정상 강화 진행
 
       // 선수 능력치 10%만큼 강화
-      await prisma.PlayerWaitingList.update({
+      await prisma.playerWaitingLists.update({
         where: {
-          playerListId: player.playerListId,
+          playerWaitingListsId: player.playerWaitingListsId,
           userId: user.userId,
           playerId: +playerId,
         },
@@ -231,14 +217,14 @@ router.patch("/reinforce/:playerId", authMiddleware, async (req, res, next) => {
     // }); //트랜잭션 마침
 
     //클라이언트에게 강화 성공한 선수의 스텟을 보여주기위해 새로 정보 불러옴
-    const forcePlayer = await prisma.PlayerWaitingList.findFirst({
+    const forcePlayer = await prisma.playerWaitingLists.findFirst({
       where: {
         userId: user.userId,
         playerId: +playerId,
       },
       select: {
         playerId: true,
-        playerName: true,
+        name: true,
         speed: true,
         goalDecisiveness: true,
         shootPower: true,
